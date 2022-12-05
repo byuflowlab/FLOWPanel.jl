9b37b535cbae54dec83600072f98ca8305f4c420
Documenter.jl <documenter@juliadocs.github.io>
2022-12-05 23:16:30 +0000

    build based on 9ac5683

--- dev/generate_examples.jl
+++ dev/generate_examples.jl
@@ -5,18 +5,19 @@ output_path = joinpath(module_path, "examples") # Path where to store  examples 
 
 # ------------- SWEPT WING EXAMPLE ---------------------------------------------
 
-input_name = "sweptwing.jl"
 output_name = "sweptwing.md"
 
 header = """
 # Swept Wing
 """
 
-break_flag = "COMPARISON TO EXPERIMENTAL DATA"
 
 open(joinpath(output_path, output_name), "w") do fout
 
     println(fout, header)
+
+    # -------- 4.2deg AOA --------------------------
+    println(fout, "## \$4.2^\\circ\$ Angle of Attack")
 
     println(fout, """
     ```@raw html
@@ -27,6 +28,9 @@ open(joinpath(output_path, output_name), "w") do fout
     """)
 
     println(fout, "```julia")
+
+    input_name = "sweptwing.jl"
+    break_flag = "COMPARISON TO EXPERIMENTAL DATA"
 
     open(joinpath(pnl.examples_path, input_name), "r") do fin
         for l in eachline(fin)
@@ -44,7 +48,7 @@ open(joinpath(output_path, output_name), "w") do fout
     (see the complete example under
     [examples/sweptwing.jl](https://github.com/byuflowlab/FLOWPanel.jl/blob/master/examples/sweptwing.jl)
     to see how to postprocess the solution to calculate the slices of pressure
-    distribution and spanwise loading that is ploted here below)
+    distribution and spanwise loading that is plotted here below)
 
     ```@raw html
     <center>
@@ -69,4 +73,47 @@ open(joinpath(output_path, output_name), "w") do fout
             println(fout, l)
         end
     end
+
+    # -------- AOA Sweep --------------------------
+    println(fout, "## AOA Sweep")
+
+    println(fout, """
+        \nUsing the wing defined in the previous section, we now sweep the angle
+        of attack.
+    """)
+
+    println(fout, "```julia")
+
+    input_name = "sweptwing_aoasweep.jl"
+    break_flag = "COMPARISON TO EXPERIMENTAL DATA"
+
+    open(joinpath(pnl.examples_path, input_name), "r") do fin
+        for l in eachline(fin)
+            if contains(l, break_flag)
+                break
+            end
+
+            println(fout, l)
+        end
+    end
+
+    println(fout, "```")
+
+    println(fout, """
+    (see the complete example under
+    [examples/sweptwing_aoasweep.jl](https://github.com/byuflowlab/FLOWPanel.jl/blob/master/examples/sweptwing_aoasweep.jl)
+    to see how to postprocess the solution as plotted here below)
+
+    ```@raw html
+    <center>
+        <br><b>Spanwise loading distribution</b>
+        <img src="../../assets/images/sweptwing000-sweep-loading.png" alt="Pic here" style="width: 100%;"/>
+
+        <br><br><b>Lift and induced drag</b>
+        <img src="../../assets/images/sweptwing000-sweep-CLCD.png" alt="Pic here" style="width: 100%;"/>
+
+        <img src="../../assets/images/sweptwing000-sweep-Cm.png" alt="Pic here" style="width: 50%;"/>
+    </center>
+    ```
+    """)
 end
