02d273f0c21f693c34c67af9e1afd5a1c8bda8ea
Documenter.jl <documenter@juliadocs.github.io>
2023-04-29 00:03:03 +0000

    build based on 4aa6c9d

--- dev/generate_examples_duct.jl
+++ dev/generate_examples_duct.jl
@@ -187,15 +187,15 @@ open(joinpath(output_path, output_name*"-leastquares.md"), "w") do fout
     This leads to an overdetermined problem where one of the original degrees of
     freedom (panels strengths) has become redundant.
 
-    Let \$n\$ the number of panels. The problem is well defined for a open
+    Let \$n\$ the number of panels. The problem is well defined for an open
     geometry formulating the solver as
     ```math
     \\begin{align*}
         G \\Gamma = -b
     ,\\end{align*}
     ```
-    where \$b \\in \\mathbb{R}^{n}\$ is the normal of the freestream condition
-    at each panel that needs to be canceled ("no-flow-through" boundary
+    where \$b \\in \\mathbb{R}^{n}\$ is the normal component of the freestream velocity
+    at each panel, which needs to be canceled ("no-flow-through" boundary
     condition), \$G \\in \\mathbb{R}^{n\\times n}\$ contains the geometric
     information of the panels and \$\\Gamma \\in \\mathbb{R}^{n}\$ is the
     strength of each vortex-ring panel.
@@ -209,8 +209,8 @@ open(joinpath(output_path, output_name*"-leastquares.md"), "w") do fout
     In order to circumvent this issue, we can transform the original
     problem into a least-squares problem as follows.
     Since one of the panel strengths is redundant in a watertight geometry,
-    we can simply pick an arbitrary panel and prescribe an arbitrary strength.
-    Then, \$G\$ has become a \$n\\times n-1\$ matrix, \$\\Gamma\$ is a
+    we can simply pick an arbitrary panel and prescribe its strength.
+    Then, \$G\$ has become a \$n\\times (n-1)\$ matrix, \$\\Gamma\$ is a
     vector of length \$n-1\$, while \$b\$ is still a vector of length \$n\$.
     To formulate the least-squares problem, we substract the velocity \$b_p\$
     induced by the "prescribed" panel  to the right-hand side,
@@ -235,7 +235,7 @@ open(joinpath(output_path, output_name*"-leastquares.md"), "w") do fout
     the original vortex-ring solver, while the least-squares solver is called
     by declaring `bodytype = pnl.RigidWakeBody{pnl.VortexRing, 2}`.
 
-    > **NOTE:** The prescribed panel can be manually set by the user through the optional argument `elprescribe = [(index, val), ...]` of `FLOWPanel.solve`, which is a list of element to prescribe, where `index` is the linear index of the element and `val` is the prescribed element strength. If not set, the function defaults to `FLOWPanel.solve(body, Uinfs, Das, Dbs; elprescribe=[(1, 0)])`
+    > **NOTE:** The prescribed panel can be manually set by the user through the optional argument `elprescribe = [(index, val), ...]` of `FLOWPanel.solve`, which is a list of elements to prescribe, where `index` is the linear index of the element and `val` is the prescribed element strength. If not set, the function defaults to `FLOWPanel.solve(body, Uinfs, Das, Dbs; elprescribe=[(1, 0)])`
 
     Even though both solvers lead to roughly the same flow field solution, the
     numerical noise of the ill-condition problem is evident when visualizing the
