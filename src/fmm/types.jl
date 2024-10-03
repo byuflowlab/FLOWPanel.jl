ac78e2ac4bf393547e20476d63e97a39805cf2fd
EdoAlvarezR <edo.alvarezr@gmail.com>
2024-10-03 17:54:42 -0500

    Add assertion AbstractPanels{TK<:AbstractKernel}; add kernel_multiplicity(<:AbstractKernel)

--- src/fmm/types.jl
+++ src/fmm/types.jl
@@ -1,10 +1,17 @@
-abstract type AbstractPanels{TK,TF,NK,NS} end
-
 abstract type AbstractKernel{M} end
 
 abstract type AbstractRotatedKernel{M} <: AbstractKernel{M} end
 
 abstract type AbstractUnrotatedKernel{M} <: AbstractKernel{M} end
 
+abstract type AbstractPanels{TK<:AbstractKernel, TF, NK, NS} end
 
 @inline kernel_multiplicity(::AbstractKernel{M}) where M = M
+
+function kernel_multiplicity(type::Type)
+    if type <: AbstractKernel
+        return type.parameters[1]
+    else
+        error("Requested kernel multiplicity over invalid type $(type)")
+    end
+end
