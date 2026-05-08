111db762bf3f639433a1f27a0068809582350b5a
Timothy Harlow <timothy@daymachine.com>
2026-05-08 14:50:42 -0600

    wrapped simulation setup and run in a function for faster iteration through meshes

--- examples/convergence_study_nc.jl
+++ examples/convergence_study_nc.jl
@@ -9,24 +9,7 @@ using VSPGeom
 import GeoIO
 using DelimitedFiles
 
-## =========================================================
-# SIMULATION PARAMETERS
-# ==========================================================
-magVinf = 0.0001    # Freestream velocity magnitude (m/s)
-AOA     = 0.0       # Angle of attack (degrees)
-rho     = 1.225     # Air density (kg/m^3)
-RPM     = 5400      # Rotation speed (rpm)
-Vinf    = magVinf * [0.0, -cosd(AOA), sind(AOA)]
-nrevs   = 10        # Number of revolutions
-nt      = 36        # Number of time steps per revolution
-dt      = 60 / RPM / nt
-n_steps = nt * nrevs
-t_range = range(0.0, step=dt, length=n_steps)
-
-## =========================================================
-# ROTOR GEOMETRY
-# ==========================================================
-function build_rotor(msh_file, te_1, te_2, R=0.119; find_te=false)
+function build_rotor(msh_file, te_1, te_2, R=0.119; find_te=false, kerneloffset=R*0.001)
 
     # MSH files
     msh  = GeoIO.load(msh_file).geometry
@@ -63,7 +46,7 @@ function build_rotor(msh_file, te_1, te_2, R=0.119; find_te=false)
 
     rotor = pnl.RigidWakeBody{kernel}(rotor.nodes, rotor.cells, [shedding1, shedding2],
                             CPoffset=1e-6,
-                            kerneloffset=R*0.001,
+                            kerneloffset=kerneloffset,
                             kernelcutoff=1e-14,
                             semiinfinite_wake=false,
                             watertight=true,
@@ -76,6 +59,112 @@ function build_rotor(msh_file, te_1, te_2, R=0.119; find_te=false)
     return rotor
 end
 
+
+function run_simulation(rotor, run_name; R=0.119, Vinf=zeros(3), RPM=5400, nt=36, nrevs=10)
+    dt      = 60 / RPM / nt
+    n_steps = nt * nrevs
+    t_range = range(0.0, step=dt, length=n_steps)
+    
+    # =========================================================
+    # WAKE SETUP
+    # ==========================================================
+    p_per_step = 2
+    overlap    = 2.0
+    wake_rotor = pnl.PanelParticleWake(rotor;
+                    nwakerows=2,
+                    max_particles=100000,
+                    method_trailing=pnl.OverlapPPS(overlap, p_per_step),
+                    method_unsteady=pnl.OverlapPPS(overlap, p_per_step),
+                    particle_maintenance=pnl.ParticleMaintenance((
+                        pnl.MergeParticles(every=1,
+                            r=R*0.02,
+                            r_hash=R*0.04,
+                            sigma_relative=false,
+                            max_sigma_ratio=2.0,
+                            skip_static=true,
+                            check_neighboring_cells=false),
+                    )))
+
+    ## =========================================================
+    # SIMULATION SETUP
+    # ==========================================================
+    Uinf(t) = Vinf
+
+    # solver_rotor = pnl.FGSSolver(rotor;
+    #             max_iterations=500,
+    #             tolerance=1.0e-6,
+    #             rlx=1.0,
+    #             expansion_order=10,
+    #             multipole_acceptance=0.4,
+    #             leaf_size=150,
+    #             shrink=true,
+    #             recenter=false,
+    #             inner_iterations=20,
+    #             reverse_pass=false,
+    #             verbose=false
+    #         )
+    solver_rotor = pnl.Backslash(rotor)
+
+    backend = pnl.FastMultipoleBackend()
+
+    # Reference frame
+    frames = pnl.ReferenceFrame(rotor;
+        origin = SVector{3}(0.0, 0.0, 0.0),
+        v = SVector{3}(0.0, 0.0, 0.0),
+        ω_axis = SVector{3}(0.0, 1.0, 0.0),
+        ω = 2*pi * RPM/60,
+        R = SMatrix{3,3}(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0),
+        name = "vehicle",
+        child_index = Int[],
+        dependent_index = [1]
+    )
+
+    # Maneuver
+    maneuver!(frames, systems, wakes, t) = nothing
+
+    ## =========================================================
+    # RUN SIMULATION
+    # ==========================================================
+    systems      = (rotor,)
+    wakes        = (wake_rotor,)
+    body_solvers = (solver_rotor,)
+    monitors = (pnl.ForceMonitor(length(t_range), 1; # un-normalized, global frame
+                        i_frame=-1, 
+                        normalization=pnl.RotorNormalization(rho, 2*R, 1)
+                    ),
+                )
+
+    nc = rotor.ncells # number of cells on this run
+
+    println("\nBegin rotor hover simulation ($(n_steps) steps, $(nc) cells)...")
+    name = run_name
+    @time pnl.simulate!(systems, wakes, frames, maneuver!, Uinf, t_range;
+        set_Das_eta_kinematic=0.1,
+        # set_Das_eta_freestream=0.1,
+        monitors,
+        body_solvers, backend, rho, verbose=true,
+        path=run_name, name
+    )
+
+    out_name = "CT_v_t_hover_RPM"*"$RPM"*"_nc$nc"*"_pps$p_per_step"*"_nt$nt"*"_overlap$overlap"*"_kerneloff0.00012"*".csv"
+    writedlm(out_name, monitors[1].force, ',')
+
+end
+
+## =========================================================
+# SIMULATION PARAMETERS
+# ==========================================================
+magVinf = 0.0001    # Freestream velocity magnitude (m/s)
+AOA     = 0.0       # Angle of attack (degrees)
+rho     = 1.225     # Air density (kg/m^3)
+RPM     = 5400      # Rotation speed (rpm)
+Vinf    = magVinf * [0.0, -cosd(AOA), sind(AOA)]
+nrevs   = 10        # Number of revolutions
+nt      = 36        # Number of time steps per revolution
+
+# =========================================================
+# ROTOR GEOMETRY
+# ==========================================================
 read_path    = joinpath(pnl.examples_path, "data")
 msh_file_1k  = joinpath(read_path, "phantom_3_1k.msh")
 msh_file_4k  = joinpath(read_path, "phantom_3_4k.msh")
@@ -101,88 +190,7 @@ rotor_4k  = build_rotor(msh_file_4k, te_4k_1, te_4k_2)
 rotor_8k  = build_rotor(msh_file_8k, te_8k_1, te_8k_2)
 rotor_16k = build_rotor(msh_file_16k, te_16k_1, te_16k_2)
 
-## =========================================================
-# WAKE SETUP
-# ==========================================================
-p_per_step = 2
-overlap    = 2.0
-
-wake_rotor = pnl.PanelParticleWake(rotor_16k;
-                nwakerows=2,
-                max_particles=100000,
-                method_trailing=pnl.OverlapPPS(overlap, p_per_step),
-                method_unsteady=pnl.OverlapPPS(overlap, p_per_step),
-                particle_maintenance=pnl.ParticleMaintenance((
-                    pnl.MergeParticles(every=1,
-                        r=R*0.02,
-                        r_hash=R*0.04,
-                        sigma_relative=false,
-                        max_sigma_ratio=2.0,
-                        skip_static=true,
-                        check_neighboring_cells=false),
-                )))
-
-## =========================================================
-# SIMULATION SETUP
-# ==========================================================
-Uinf(t) = Vinf
-
-# solver_rotor = pnl.FGSSolver(rotor;
-#             max_iterations=500,
-#             tolerance=1.0e-6,
-#             rlx=1.0,
-#             expansion_order=10,
-#             multipole_acceptance=0.4,
-#             leaf_size=150,
-#             shrink=true,
-#             recenter=false,
-#             inner_iterations=20,
-#             reverse_pass=false,
-#             verbose=false
-#         )
-solver_rotor = pnl.Backslash(rotor_16k)
-
-backend = pnl.FastMultipoleBackend()
-
-# Reference frame
-frames = pnl.ReferenceFrame(rotor_16k;
-    origin = SVector{3}(0.0, 0.0, 0.0),
-    v = SVector{3}(0.0, 0.0, 0.0),
-    ω_axis = SVector{3}(0.0, 1.0, 0.0),
-    ω = 2*pi * RPM/60,
-    R = SMatrix{3,3}(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0),
-    name = "vehicle",
-    child_index = Int[],
-    dependent_index = [1]
-)
-
-# Maneuver
-maneuver!(frames, systems, wakes, t) = nothing
-
-## =========================================================
+# =========================================================
 # RUN SIMULATION
 # ==========================================================
-systems      = (rotor_16k,)
-wakes        = (wake_rotor,)
-body_solvers = (solver_rotor,)
-monitors = (pnl.ForceMonitor(length(t_range), 1; # un-normalized, global frame
-                    i_frame=-1, 
-                    normalization=pnl.RotorNormalization(rho, 2*R, 1)
-                ),
-            )
-
-nc = rotor_16k.ncells # number of cells on this run
-
-println("\nBegin rotor hover simulation ($(n_steps) steps, $(nc) cells)...")
-name = "rotor_hover_8k"
-@time pnl.simulate!(systems, wakes, frames, maneuver!, Uinf, t_range;
-    set_Das_eta_kinematic=0.1,
-    # set_Das_eta_freestream=0.1,
-    monitors,
-    body_solvers, backend, rho, verbose=true,
-    path="rotor_hover_8k", name
-)
-
-
-out_name = "CT_v_t_hover_RPM"*"$RPM"*"_nc$nc"*"_pps$p_per_step"*"_nt$nt"*"_overlap$overlap"*"_kerneloff0.00012"*".csv"
-writedlm(out_name, monitors[1].force, ',')
+run_simulation(rotor_1k, "rotor_hover_1k"; Vinf=Vinf)
