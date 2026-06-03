6a03ab69220a0437e377e3aa573a00d0264ea044
Ryan Anderson <rymanderson@gmail.com>
2026-06-03 09:23:12 -0600

    prep workstation run

--- examples/rotor_hover_pressure_comparison.jl
+++ examples/rotor_hover_pressure_comparison.jl
@@ -137,9 +137,9 @@ println("  shedding2 root midpoint r/R = $(shedding_root_r_over_R(rotor.nodes, s
 wake_rotor = pnl.PanelParticleWake(rotor;
     nwakerows=1, max_particles=500_000, core_size=wake_core_size,
     particle_kerneloffset=kerneloffset_targets,
-    # viscous=pnl.FLOWVPM.CoreSpreading(wake_nu, wake_core_size, pnl.FLOWVPM.zeta_fmm;
-        # beta=wake_core_beta),
-    # SFS=pnl.FLOWVPM.SFS_Cd_twolevel_nobackscatter,
+    viscous=pnl.FLOWVPM.CoreSpreading(wake_nu, wake_core_size, pnl.FLOWVPM.zeta_fmm;
+        beta=wake_core_beta),
+    SFS=pnl.FLOWVPM.SFS_Cd_twolevel_nobackscatter,
     # method_trailing=pnl.SigmaOverlap(R*0.05, 4.0),
     method_trailing=pnl.OverlapPPS(overlap, 2),
     method_unsteady=pnl.NoShed(),
